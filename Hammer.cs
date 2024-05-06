7b1d046dffbcbde160be2bfb9c5824aef1eef74c
KevinYakha <kevin@yakha.eu>
2024-05-06 14:11:04 +0200

    Added constructors to set Hammer and Shield attributes

--- Hammer.cs
+++ Hammer.cs
@@ -4,6 +4,11 @@
     {
         public int attackValue { get; set; }
 
+        public Hammer(int attackValue)
+        {
+            this.attackValue = attackValue;
+        }
+
         public override void useItem()
         {
             Console.WriteLine($"You attack with your hammer, dealing {attackValue} damage.");
--- Shield.cs
+++ Shield.cs
@@ -4,6 +4,11 @@
     {
         public int defenseValue {  get; set; }
 
+        public Shield(int defenseValue)
+        {
+            this.defenseValue = defenseValue;
+        }
+
         public override void useItem()
         {
             Console.WriteLine($"You defend yourself with your shield, mitigating {defenseValue} damage.");

c8a596b4c2ffa83438198855b34213dc3547a845
KevinYakha <kevin@yakha.eu>
2024-05-06 13:40:27 +0200

    abstract class Item added

---
+++ Item.cs
@@ -0,0 +1,10 @@
+﻿namespace FactoryMethodPattern
+{
+    public abstract class Item
+    {
+        public string name { get; set; }
+        public int id { get; set; }
+
+        public abstract void useItem();
+    }
+}

e41aab67f7a4af99bf6af5c55e004b1552770aca
KevinYakha <kevin@yakha.eu>
2024-05-07 22:32:42 +0200

    Added MobCreator abstract Factory

---
+++ MobCreator.cs
@@ -0,0 +1,12 @@
+﻿namespace FactoryMethodPattern
+{
+    public abstract class MobCreator<TEnum> where TEnum : Enum
+    {
+        protected abstract Mob mobFactory(TEnum mobType);
+
+        public Mob createMob(TEnum mobType)
+        {
+            return mobFactory(mobType);
+        }
+    }
+}

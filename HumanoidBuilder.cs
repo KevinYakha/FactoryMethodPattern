29d5b1e5afe305feb8c304a9801768a1fe903f3b
KevinYakha <kevin@yakha.eu>
2024-05-07 22:33:38 +0200

    Added Humanoid Factory

---
+++ HumanoidBuilder.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace FactoryMethodPattern
+{
+    internal class HumanoidBuilder : HumanoidCreator
+    {
+        protected override Humanoid mobFactory(HumanoidType humanoidType)
+        {
+            switch (humanoidType)
+            {
+                case HumanoidType.Human:
+                    return new Human();
+                case HumanoidType.Goblin:
+                    return new Goblin();
+                default:
+                    throw new ArgumentException();
+            }
+        }
+    }
+}
---
+++ HumanoidCreator.cs
@@ -0,0 +1,11 @@
+﻿namespace FactoryMethodPattern
+{
+    public abstract class HumanoidCreator : MobCreator<HumanoidType>
+    {
+        protected override abstract Humanoid mobFactory(HumanoidType humanoidType);
+        public Humanoid createHumanoid(HumanoidType humanoidType)
+        {
+            return mobFactory(humanoidType);
+        }
+    }
+}

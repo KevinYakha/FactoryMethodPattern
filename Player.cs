bb8dc01eaff650395bafca847d7775950af4d8b1
KevinYakha <kevin@yakha.eu>
2024-05-07 15:21:14 +0200

    Singleton Player was added

---
+++ Player.cs
@@ -0,0 +1,32 @@
+﻿namespace FactoryMethodPattern
+{
+    public class Player
+    {
+        private static readonly Player player = new Player();
+
+        public int health;
+        private List<Item> inventory { get; } = new List<Item>();
+
+        private Player() {}
+
+        public static Player getInstance()
+        {
+            return player;
+        }
+
+        public void addItemToInventory(Item item)
+        {
+            inventory.Add(item);
+        }
+
+        public void removeItemFromInventory(int id)
+        {
+            inventory.RemoveAt(inventory.FindIndex(x => x.id == id));
+        }
+
+        public void removeItemFromInventory(Item item)
+        {
+            inventory.Remove(item);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@
     {
         public static void Main(string[] args)
         {
+            Player player = Player.getInstance();
+
             ItemCreator inventory = new itemBuilder();
 
             Item random1 = inventory.createRandomItem();

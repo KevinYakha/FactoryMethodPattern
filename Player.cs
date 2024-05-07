d4585a0e03e9009fa4dffa922b36b0e9007cca9d
KevinYakha <kevin@yakha.eu>
2024-05-07 16:03:17 +0200

    Made Player a Humanoid Mob and implemented it's abstract methods

--- Player.cs
+++ Player.cs
@@ -1,17 +1,37 @@
 ﻿namespace FactoryMethodPattern
 {
-    public class Player
+    public class Player : Humanoid 
     {
         private static readonly Player player = new Player();
 
-        public Inventory inventory = new Inventory();
-        public int health;
-
-        private Player() {}
+        private Player()
+        {
+            health = 100;
+        }
 
         public static Player getInstance()
         {
             return player;
+        }
+
+        public override string getDescription()
+        {
+            return "This is the player character";
+        }
+
+        public override string speak()
+        {
+            return "I am that guy!";
+        }
+
+        public override void attack(Mob target)
+        {
+            target.health -= 5;
+        }
+        
+        public override void attack(Mob target, Item weapon)
+        {
+            target.health -= weapon.getAttribute();
         }
     }
 }

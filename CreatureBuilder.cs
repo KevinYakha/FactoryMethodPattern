c043d408b8d0b5dfbd8fc6216bd3dc7557310fa5
KevinYakha <kevin@yakha.eu>
2024-05-07 22:33:53 +0200

    Added Creature Factory

---
+++ CreatureBuilder.cs
@@ -0,0 +1,16 @@
+﻿namespace FactoryMethodPattern
+{
+    public class CreatureBuilder : CreatureCreator
+    {
+        protected override Creature mobFactory(CreatureType creatureType)
+        {
+            switch (creatureType)
+            {
+                case CreatureType.Wolf:
+                    return new Wolf();
+                default:
+                    throw new ArgumentException();
+            }
+        }
+    }
+}
---
+++ CreatureCreator.cs
@@ -0,0 +1,11 @@
+﻿namespace FactoryMethodPattern
+{
+    public abstract class CreatureCreator : MobCreator<CreatureType>
+    {
+        protected override abstract Creature mobFactory(CreatureType creatureType);
+        public Creature createCreature(CreatureType creatureType)
+        {
+            return mobFactory(creatureType);
+        }
+    }
+}

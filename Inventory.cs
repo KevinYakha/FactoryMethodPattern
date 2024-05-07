f5c4c4fe7224babd0e024a688974e09924d59320
KevinYakha <kevin@yakha.eu>
2024-05-07 15:34:54 +0200

    Separated inventory from player class

---
+++ Inventory.cs
@@ -0,0 +1,22 @@
+﻿namespace FactoryMethodPattern
+{
+    public class Inventory
+    {
+        public List<Item> inventory = new List<Item>();
+
+        public void addItem(Item item)
+        {
+            inventory.Add(item);
+        }
+
+        public void removeItem(int id)
+        {
+            inventory.RemoveAt(inventory.FindIndex(x => x.id == id));
+        }
+
+        public void removeItem(Item item)
+        {
+            inventory.Remove(item);
+        }
+    }
+}
--- Player.cs
+++ Player.cs
@@ -4,29 +4,14 @@
     {
         private static readonly Player player = new Player();
 
+        public Inventory inventory = new Inventory();
         public int health;
-        private List<Item> inventory { get; } = new List<Item>();
 
         private Player() {}
 
         public static Player getInstance()
         {
             return player;
-        }
-
-        public void addItemToInventory(Item item)
-        {
-            inventory.Add(item);
-        }
-
-        public void removeItemFromInventory(int id)
-        {
-            inventory.RemoveAt(inventory.FindIndex(x => x.id == id));
-        }
-
-        public void removeItemFromInventory(Item item)
-        {
-            inventory.Remove(item);
         }
     }
 }

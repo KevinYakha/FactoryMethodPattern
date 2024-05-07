1db6abbe7984a18c309ca6f00823cfc829b37d55
KevinYakha <kevin@yakha.eu>
2024-05-07 16:19:24 +0200

    Added Goblin Humanoid type

---
+++ Goblin.cs
@@ -0,0 +1,29 @@
+﻿namespace FactoryMethodPattern
+{
+    public class Goblin : Humanoid
+    {
+        public Goblin()
+        {
+            health = 25;
+        }
+        public override void attack(Mob target)
+        {
+            target.health -= 5;
+        }
+
+        public override void attack(Mob target, Item weapon)
+        {
+            target.health -= weapon.getAttribute();
+        }
+
+        public override string getDescription()
+        {
+            return "It is a goblin.";
+        }
+
+        public override string speak()
+        {
+            return "Grumble, grumble...";
+        }
+    }
+}

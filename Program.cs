d7f4074e7f3b4ffbd6f93ca0fb28a4a48de60909
KevinYakha <kevin@yakha.eu>
2024-05-07 22:34:22 +0200

    Added code for testing purposes to the main function

--- Program.cs
+++ Program.cs
@@ -5,14 +5,27 @@
         public static void Main(string[] args)
         {
             Player player = Player.getInstance();
+            ItemCreator itemCreator = new itemBuilder();
 
-            ItemCreator inventory = new itemBuilder();
+            player.inventory.addItem(itemCreator.createItem(ItemType.Hammer, 30));
 
-            Item random1 = inventory.createRandomItem();
-            Item random2 = inventory.createRandomItem();
-            Item random3 = inventory.createRandomItem();
-            Item random4 = inventory.createRandomItem();
-            Item random5 = inventory.createRandomItem();
+            HumanoidCreator humanoidCreator = new HumanoidBuilder();
+            CreatureCreator creatureCreator = new CreatureBuilder();
+
+            Human human = (Human)humanoidCreator.createHumanoid(HumanoidType.Human);
+            Goblin goblin = (Goblin)humanoidCreator.createHumanoid(HumanoidType.Goblin);
+            Wolf wolf = (Wolf)creatureCreator.createCreature(CreatureType.Wolf);
+
+            human.inventory.addItem(itemCreator.createItem(ItemType.Hammer, 20));
+
+            human.attack(wolf, human.inventory.inventory[0]);
+            Console.WriteLine("wolf health: {0}", wolf.health);
+
+            wolf.attack(goblin);
+            Console.WriteLine("goblin health: {0}", goblin.health);
+
+            goblin.attack(human);
+            Console.WriteLine("human health: {0}", human.health);
 
             Console.ReadKey();
         }
